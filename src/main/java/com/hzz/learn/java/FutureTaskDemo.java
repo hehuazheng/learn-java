44e658e46e21a54eaa9b035e24b66dc3266b745a
hehuazheng <avaforreg@163.com>
2016-05-01 22:50:02 +0800

    init

---
+++ src/main/java/com/hzz/learn/java/FutureTaskDemo.java
@@ -0,0 +1,27 @@
+package com.hzz.learn.java;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class FutureTaskDemo {
+
+	public static void main(String[] args) throws InterruptedException,
+			ExecutionException {
+		FutureTask<String> fTask = new FutureTask<String>(
+				new Callable<String>() {
+					@Override
+					public String call() throws Exception {
+						System.out.println("in call start");
+						Thread.sleep(3000L);
+						System.out.println("in call end");
+						return "called";
+					}
+				});
+		fTask.run();
+		System.out.println("after future task run");
+		String result = fTask.get();
+		System.out.println(result);
+	}
+
+}
