58f35187879b1f4db12469907ed53b12bc1acf88
hehuazheng <avaforreg@163.com>
2016-05-14 13:48:12 +0800

    add

---
+++ src/main/java/com/hzz/learn/java/CountDownLatchDemo.java
@@ -0,0 +1,39 @@
+package com.hzz.learn.java;
+
+import java.util.concurrent.CountDownLatch;
+
+public class CountDownLatchDemo {
+
+	public static void main(String[] args) throws InterruptedException {
+		int n = 3;
+		CountDownLatch cdl = new CountDownLatch(n);
+		for (int i = 0; i < n; i++) {
+			new MyThread(cdl).start();
+		}
+		System.out.println("before cound down latch");
+		cdl.await();
+		System.out.println("task completed");
+	}
+
+	static class MyThread extends Thread {
+		private CountDownLatch cdl;
+
+		public MyThread(CountDownLatch cdl) {
+			this.cdl = cdl;
+		}
+
+		@Override
+		public void run() {
+			try {
+				Thread.sleep(2000L);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println(Thread.currentThread().getName()
+					+ " sleep 2 seconds");
+			cdl.countDown();
+		}
+	}
+
+}
---
+++ src/main/java/com/hzz/learn/java/ScheduledJobDemo.java
@@ -0,0 +1,27 @@
+package com.hzz.learn.java;
+
+import java.lang.management.ManagementFactory;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ScheduledJobDemo {
+
+	public static void main(String[] args) {
+		ScheduledExecutorService ses = Executors
+				.newScheduledThreadPool(ManagementFactory
+						.getOperatingSystemMXBean().getAvailableProcessors());
+		ses.scheduleWithFixedDelay(new Runnable() {
+			@Override
+			public void run() {
+				Date date = new Date();
+				SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+				System.out.println(sdf.format(date));
+			}
+		}, 3L, 3L, TimeUnit.SECONDS);
+
+	}
+
+}
