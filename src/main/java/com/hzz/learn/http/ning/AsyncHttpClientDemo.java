3434f1fc9b3426b6c792549ec79a38fbd94533c2
hejiafu <hejiafu@secoo.com>
2016-06-16 19:52:57 +0800

    add ning demo

---
+++ src/main/java/com/hzz/learn/http/ning/AsyncHttpClientDemo.java
@@ -0,0 +1,20 @@
+package com.hzz.learn.http.ning;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.Response;
+
+public class AsyncHttpClientDemo {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException, IOException {
+		AsyncHttpClient ahc = new AsyncHttpClient();
+		Future<Response> f = ahc.prepareGet("http://www.baidu.com").execute();
+		Response r = f.get();
+		System.out.println(r.getResponseBody());
+		ahc.close();
+	}
+
+}
